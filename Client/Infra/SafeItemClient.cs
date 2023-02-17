9341289b8a18868eab844150b43ce22325c1cd15
Hugh Maaskant <hugh.maaskant@outlook.com>
2023-02-17 15:33:27 +0100

    Add JsonPatch on Client side

--- Client/Infra/SafeItemClient.cs
+++ Client/Infra/SafeItemClient.cs
@@ -1,19 +1,24 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using Microsoft.AspNetCore.JsonPatch;
+
 using BlazorPatchDemo.Shared;
 using BlazorPatchDemo.Shared.Dtos;
 using BlazorPatchDemo.Shared.Entities;
+using Newtonsoft.Json;
 
 namespace BlazorPatchDemo.Client.Infra;
 
 internal sealed class SafeItemClient
 {
     private const string ApplicationJson = "application/json";
+    private const string ApplicationJsonPatch = "application/json-patch+json";
 
     private readonly ILogger<SafeItemClient> _logger;
     private readonly HttpClient _client;
 
-    
+
     public SafeItemClient(ILogger<SafeItemClient> logger, HttpClient client)
     {
         _logger = logger;
@@ -52,23 +57,24 @@ internal sealed class SafeItemClient
                     return ApiResult<List<Item>>.Fail(
                         "Cannot deserialize response to List<ItemDto>", response.StatusCode);
                 }
-                
+
                 // Convert every received ItemDto to an Item
                 List<Item> items = dtos.Select(dto => dto.ToItem()).ToList();
-                
+
                 _logger.LogInformation("Retrieved {Count} Items", items.Count);
                 return ApiResult<List<Item>>.Success(items, response.StatusCode);
             }
 
             _logger.LogError(
                 "Error retrieving Items from the Server; StatusCode = {StatusCode}", response.StatusCode);
-            return ApiResult<List<Item>>.Fail("Error Getting Items from the Server", response.StatusCode);
+            return ApiResult<List<Item>>.Fail("Error retrieving Items from the Server", response.StatusCode);
         }
         catch (HttpRequestException e)
         {
-            _logger.LogError("Exception retrieving Items from Server, Message=\"{Message}\"; StatusCode={StatusCode}", 
+            _logger.LogError("Exception retrieving Items from Server, Message=\"{Message}\"; StatusCode={StatusCode}",
                 e.Message, e.StatusCode);
-            return ApiResult<List<Item>>.Fail($"Exception retrieving Items from Server, Message=\"{e.Message}\"", e.StatusCode);
+            return ApiResult<List<Item>>.Fail($"Exception retrieving Items from Server, Message=\"{e.Message}\"",
+                e.StatusCode);
         }
     }
 
@@ -91,7 +97,7 @@ internal sealed class SafeItemClient
         try
         {
             ItemToCreateDto itemToCreateDto = item.AsCreateItemDto();
-            
+
             var response = await _client.PostAsJsonAsync("/items", itemToCreateDto, token);
             if (response.IsSuccessStatusCode)
             {
@@ -100,10 +106,10 @@ internal sealed class SafeItemClient
                 if (dto is not null)
                 {
                     item = dto.ToItem();
-                    
+
                     _logger.LogInformation(
                         "Item \"{Name}\" with Id {Id} created at Server on {CreatedDate} at {CreatedTime}",
-                        dto.Name, 
+                        dto.Name,
                         item.Id,
                         item.CreatedDate.ToString("yyyy-MM-dd"),
                         item.CreatedDate.ToString("hh:mm:ss"));
@@ -112,7 +118,8 @@ internal sealed class SafeItemClient
 
                 _logger.LogError("Error deserializing Item \"{Name}\"; Status = {StatusCode},",
                     itemToCreateDto.Name, response.StatusCode);
-                return ApiResult<Item>.Fail($"Error deserializing Item \"{itemToCreateDto.Name}\"", response.StatusCode);
+                return ApiResult<Item>.Fail($"Error deserializing Item \"{itemToCreateDto.Name}\"",
+                    response.StatusCode);
             }
 
             _logger.LogError("Error creating Item \"{Name}\"; Status = {StatusCode},",
@@ -121,12 +128,12 @@ internal sealed class SafeItemClient
         }
         catch (HttpRequestException e)
         {
-            _logger.LogError("Exception creating Item \"{Name}\"; Message = \"{Message}\"; Status = {StatusCode}",
+            _logger.LogError("Exception creating Item \"{Name}\", Message = \"{Message}\"; Status = {StatusCode}",
                 item.Name, e.Message, e.StatusCode ?? HttpStatusCode.Unused);
-            return ApiResult<Item>.Fail($"Exception creating {item.Name}; Message=\"{e.Message}\".", e.StatusCode);
+            return ApiResult<Item>.Fail($"Exception creating Item {item.Name}; Message = \"{e.Message}\"", e.StatusCode);
         }
     }
-    
+
     /// <summary>
     /// Use HTTP PUT to update an <see cref="Item"/> on the Server
     /// </summary>
@@ -141,7 +148,8 @@ internal sealed class SafeItemClient
 
         try
         {
-            HttpResponseMessage response = await _client.PutAsJsonAsync($"items/{item.Id}", dto, cancellationToken: token);
+            HttpResponseMessage response =
+                await _client.PutAsJsonAsync($"items/{item.Id}", dto, cancellationToken: token);
             if (response.IsSuccessStatusCode)
             {
                 _logger.LogInformation("Item \"{Name}\" has been updated at {Path}",
@@ -151,14 +159,82 @@ internal sealed class SafeItemClient
 
             _logger.LogError("Error updating Item \"{Name}\" at {Path}, Status = {StatusCode},",
                 dto.Name, $"items/{item.Id}", response.StatusCode);
-            return ApiResult<Item>.Fail($"Error Putting Item \"{dto.Name}\" at items/{item.Id}", response.StatusCode);
+            return ApiResult<Item>.Fail($"Error Putting Item \"{dto.Name}\" with Id {item.Id}", response.StatusCode);
         }
         catch (HttpRequestException e)
         {
-            _logger.LogError("Error updating Item \"{Name}\" at {Path}, Message = {Message}; Status = {StatusCode}",
-                dto.Name, $"items/{item.Id}", e.Message, e.StatusCode);
+            _logger.LogError("Exception updating Item \"{Name}\" with Id {Id}, Message = \"{Message}\"; Status = {StatusCode}",
+                dto.Name, item.Id, e.Message, e.StatusCode);
             return ApiResult<Item>.Fail(
-                $"Exception updating {dto.Name} at at items/{item.Id}, Message=\"{e.Message}\".", e.StatusCode);
+                $"Exception updating Item {dto.Name} with Id {item.Id}, Message = \"{e.Message}\"", e.StatusCode);
+        }
+    }
+
+    /// <summary>
+    /// Use HTTP PATCH to partially update an <see cref="Item"/> on the Server.
+    /// </summary>
+    /// <param name="original">The original <see cref="Item"/>.</param>
+    /// <param name="modified">The updated <see cref="Item"/>.</param>
+    /// <param name="token">
+    /// A cancellation token that can be used by other objects or threads to receive notice of cancellation.
+    /// </param>
+    /// <returns>
+    /// An <see cref="ApiResult{T}"/> of <see cref="Item"/> with the representation of the Item on the Server.
+    /// </returns>
+    /// <exception cref="ArgumentException"></exception>
+    internal async Task<ApiResult<Item>> PatchItemAsync(Item original, Item modified, CancellationToken token = default)
+    {
+        if (modified.Id != original.Id)
+            throw new ArgumentException("PatchItemsAsync called with different Ids for updated and original Items");
+
+        JsonPatchDocument<Item> patchDocument = new JsonPatchDocument<Item>();
+
+        // Naive creation of the JsonPatchDocument
+        if (modified.Name != original.Name)
+            patchDocument.Replace(path => path.Name, modified.Name);
+        if (modified.Description != original.Description)
+            patchDocument.Replace(path => path.Description, modified.Description);
+        if (modified.Price != original.Price)
+            patchDocument.Replace(path => path.Price, modified.Price);
+
+        int patchCount = patchDocument.Operations.Count;
+
+        if (patchCount == 0)
+        {
+            _logger.LogWarning("PatchItemAsync called but the updated and original Items are Identical");
+            return ApiResult<Item>.Success(original, HttpStatusCode.NotModified);
+        }
+
+        try
+        {
+            // Use NewtonSoft converter in stead of the System.Text.Json 
+            var serializedPatch = JsonConvert.SerializeObject(patchDocument);
+            var requestContent = new StringContent(serializedPatch, Encoding.UTF8, ApplicationJsonPatch);
+
+            // Note: PatchAsJsonAsync will use "application/json" in stead of "application/json-patch+json";
+            var response = await _client.PatchAsync(
+                $"items/{original.Id}", requestContent, cancellationToken: token);
+
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogInformation("Item \"{Name} with Id {Id} has been patched with {Count} replacements",
+                    original.Name, original.Id, patchCount);
+                return ApiResult<Item>.Success(modified, response.StatusCode);
+            }
+
+            _logger.LogError("Error patching Item \"{Name}\" with Id {Id}; Status = {StatusCode}",
+                original.Name, original.Id, response.StatusCode);
+            return ApiResult<Item>.Fail(
+                $"Error patching Item \"{original.Name}\" with Id {original.Id}", response.StatusCode);
+        }
+        catch (HttpRequestException e)
+        {
+            _logger.LogError(
+                "Exception patching Item \"{Name}\" with Id {Id}, Message = \"{Message}\"; Status = {StatusCode}",
+                original.Name, original.Id, e.Message, e.StatusCode);
+            return ApiResult<Item>.Fail(
+                $"Exception patching Item \"{original.Name}\" with Id {original.Id}), Message=\"{e.Message}\"",
+                e.StatusCode);
         }
     }
 
@@ -188,16 +264,18 @@ internal sealed class SafeItemClient
             }
 
             _logger.LogError("Error deleting Item \"{Name}\" with Id {Id}; Status = {StatusCode}",
-                 item.Name, item.Id, response.StatusCode);
-            return ApiResult<Item>.Fail($"Error deleting Item \"{item.Name}\" ({item.Id})", response.StatusCode);
+                item.Name, item.Id, response.StatusCode);
+            return ApiResult<Item>.Fail($"Error deleting Item \"{item.Name}\" with Id {item.Id})", response.StatusCode);
         }
         catch (HttpRequestException e)
         {
-            _logger.LogError("Exception deleting Item \"{Name}\" with Id {Id}, Message = {Message}; Status = {StatusCode}",
-                 item.Name, item.Id, e.Message, e.StatusCode);
+            _logger.LogError(
+                "Exception deleting Item \"{Name}\" with Id {Id}, Message = \"{Message}\"; Status = {StatusCode}",
+                item.Name, item.Id, e.Message, e.StatusCode);
             return ApiResult<Item>.Fail(
-                $"Exception deleting Item \"{item.Name}\" with {nameof(Item.Id)} {item.Id}); Message=\"{e.Message}\".",
+                $"Exception deleting Item \"{item.Name}\" with Id {item.Id}), Message = \"{e.Message}\"",
                 e.StatusCode);
         }
     }
+
 }
