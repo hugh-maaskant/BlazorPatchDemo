9eaac6d0015923f2e9e3c57f2981dac053a3bef7
Hugh Maaskant <hugh.maaskant@outlook.com>
2023-02-20 16:35:29 +0100

    Add null checks in the startup code for the Server

--- Server/MongoDB/Extensions.cs
+++ Server/MongoDB/Extensions.cs
@@ -18,8 +18,18 @@ public static class Extensions
         services.AddSingleton(serviceProvider =>
         {
             var configuration = serviceProvider.GetService<IConfiguration>();
+            if (configuration is null)
+                throw new Exception("Fatal: cannot obtain the Configuration values");
+            
             var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+            if (serviceSettings is null)
+                throw new Exception("Fatal: cannot obtain the Service Settings");
+
             var mongoDbSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            if (mongoDbSettings is null)
+                throw new Exception("Fatal: cannot obtain the Mongo Settings");
+            
+            // The following operations will either succeed or throw an exception
             var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
             return mongoClient.GetDatabase(serviceSettings.ServiceName);
         });
@@ -33,6 +43,9 @@ public static class Extensions
         services.AddSingleton<IRepository<T>>(serviceProvider =>
         {
             var database = serviceProvider.GetService<IMongoDatabase>();
+            if (database is null)
+                throw new Exception("Fatal: cannot obtain the Mongo Database");
+            
             return new MongoRepository<T>(database, collectionName);
         });
 
